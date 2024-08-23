0886f01c29f45163675e79afb154180650a4bbf4
Julián Castro <julianchoalt64@gmail.com>
2024-08-22 19:49:32 -0500

    se realiza la revision final del codigo para entrega

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const textarea = document.querySelector(".text-area"); 
 const mensaje = document.querySelector(".mensaje");
 
-//La letra "e" es convertida para "enter"
-//La letra "i" es convertida para "imes"
-//La letra "a" es convertida para "ai"
-//La letra "o" es convertida para "ober"
-//La letra "u" es convertida para "ufat"
-
 function bencriptador(){
     const textoencriptado = encriptar(textarea.value)
     mensaje.value = textoencriptado
